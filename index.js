f7c1423b64ae623a616b22827c5b9b7a8439ba1f
Christopher Carr <christopher.andrew.carr@gmail.com>
2017-11-18 20:35:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+class BoardMember {
+  constructor(name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.trainin = training;
+  }
+  
+  veto() {
+    return 'No, I must disagree';
+  }
+  
+  approve() {
+    return 'You can do that!';
+  }
+  
+  doCharity() {
+    return 'I like people.';
+  }
+  
+  releasePressStatement() {
+    return 'You will see great things from Scuber.'
+  }
+  
+  
+}
+
